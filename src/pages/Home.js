c1aca85d826ea0cb1e5a991a01983cd991424b7e
sea <sea11rod@gmail.com>
2024-01-26 13:06:38 +0530

    brochure download

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,21 @@ import "./Home.css";
 
 export default function Home() {
   const [eventsData, updateEvent] = useState([]);
+  const [brochure, setbrochure] = useState("");
 
   useEffect(() => {
     axiosInstance.get("events/event/1/").then((res) => {
       updateEvent(res.data.sub_event);
+      setbrochure(res.data.brochure)
       console.log(eventsData);
     });
   }, []);
   return (
     <>
+      <a href={brochure} download="your_filename.pdf">
+        Download PDF
+      </a>
+
       <h1 className="custom-font title">EVENTS</h1>
       {eventsData.map((event) => (
         <EventCard key={event.id} event={event} />
